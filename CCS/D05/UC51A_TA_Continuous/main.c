21755cd7dbc44e52750354507de4de3a7fe7d01b
Patriks Raiss-Reiss <64326465+l0tys@users.noreply.github.com>
2025-12-30 22:52:06 +0200

    UC51

--- CCS/D05/UC51A_TA_Continuous/main.c
+++ CCS/D05/UC51A_TA_Continuous/main.c
@@ -1,47 +1,46 @@
 #include <msp430.h>
-void main(void) {
-  WDTCTL = WDTPW + WDTHOLD;
-  P1DIR = 0b01000001;
-  P1OUT = 0b01000001;
-  
-  // Low frequency mode of LFXT1
-  BCSCTL1 &= ~XTS;
-  // Turn on LFXT1 (32.768kHz crystal)
-  BCSCTL3 &= ~(LFXT1S1 | LFXT1S0);
-  
-  // 2^DIVA=1 -> no ACLK divider
-  BCSCTL1 &= ~(DIVA1 | DIVA0);
-  
-  // Wait for LFXT1 to stabilize
-  _BIC_SR(OSCOFF);
-  while (IFG1 & OFIFG) {
-    IFG1 &= ~OFIFG;
-    __delay_cycles(10000);
-  }
-  
-  // Start TA0 in CONTINUOUS mode with ACLK
-  TA0CTL = TASSEL_1 + ID_2 + MC_2 + TACLR;
-  TA0CCR0 = 10714;
-  TA0CCTL0 = CCIE;
-  
-  // Start TA1 in CONTINUOUS mode with ACLK
-  TA1CTL = TASSEL_1 + ID_2 + MC_2 + TACLR;
-  TA1CCR0 = 5000;
-  TA1CCTL0 = CCIE;
-  
-  _BIS_SR(GIE+CPUOFF);
+
+void main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;
+    
+    P1DIR = 0b01000001;
+    P1OUT = 0b01000000;
+    
+    P2SEL |= BIT6 | BIT7;
+    BCSCTL1 &= ~XTS;
+    BCSCTL3 = XCAP_3;
+    BCSCTL3 &= ~(LFXT1S1 | LFXT1S0);
+    BCSCTL1 &= ~(DIVA1 | DIVA0);
+    
+    __bic_SR_register(OSCOFF);
+    
+    volatile unsigned int i;
+    do {
+        IFG1 &= ~OFIFG;
+        for(i = 50000; i > 0; i--);
+    } while (IFG1 & OFIFG);
+    
+    TA0CTL = TASSEL_1 + ID_2 + MC_2 + TACLR;
+    TA0CCR0 = 12288;
+    TA0CCTL0 = CCIE;
+
+    TA0CCR1 = 5734;
+    TA0CCTL1 = CCIE;
+    
+    __enable_interrupt();
+    _BIS_SR(LPM3_bits + GIE);
 }
 
 #pragma vector=TIMER0_A0_VECTOR
-__interrupt void TA0CCR0_isr(void) {
-  TA0CCR0 += 10714;
-  TA0CCTL0 &= ~CCIFG;
-  P1OUT ^= BIT6;
+__interrupt void Timer_A0_ISR(void) {
+    TA0CCR0 += 12288;
+    P1OUT ^= BIT6;
 }
 
-#pragma vector=TIMER1_A1_VECTOR
-__interrupt void TA1CCR0_isr(void) {
-  TA0CCR1 += 5000;
-  TA0CCTL1 &= ~CCIFG;
-  P1OUT ^= BIT0;
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void Timer_A1_ISR(void) {
+    TA0CCR1 += 5734;
+    TA0CCTL1 &= ~CCIFG;
+    P1OUT ^= BIT0;
 }
--- CCS/D05/UC51C_TA_UPDown/main.c
+++ CCS/D05/UC51C_TA_UPDown/main.c
@@ -1,40 +1,33 @@
 #include <msp430.h>
 
 void main(void) {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  P1DIR = 0b01000001;
-  P1OUT = 0b01000001;
-
-  BCSCTL1 = 0b10000000;
-  DCOCTL = 0b00000000;
-
-  // 2^DIVA=2^0=1 -> f_ACKL = f_LFXT1 / 1 T_aclk=2*T_LFXT1
-  BCSCTL2 &= ~(DIVA1 | DIVA0);
-
-  // Start TA0 in up/down mode with T_TAclk=4T_aclk
-  TA0CTL = TASSEL_2 + ID_2 + MC_3 + TACLR;
-  // Eanble interrupts at TA0R = TA0CCR0
-  TA0CCR0 = 18750;
-  TA0CCTL0 = CCIE;
-
-  // Eanble interrupts at TA1R = TA1CCR0
-  TA0CCR1 = 8750;
-  TA0CCTL1 = CCIE;
-
-  // Sleep mode with TA0/TA1 CCR0 interrupts
-  _BIS_SR(GIE+CPUOFF);
+    WDTCTL = WDTPW | WDTHOLD;
+    
+    P1DIR = 0b01000001;
+    P1OUT = 0b01000000;
+    
+    BCSCTL1 = 0b10000000;  
+    DCOCTL = 0b00000000;   
+    BCSCTL2 = 0;         
+    
+    TA0CTL = TASSEL_2 + ID_2 + MC_3 + TACLR;
+    TA0CCR0 = 18750;
+    TA0CCTL0 = CCIE;
+    
+    TA1CTL = TASSEL_2 + ID_2 + MC_3 + TACLR;
+    TA1CCR0 = 8750;
+    TA1CCTL0 = CCIE;
+    
+    __enable_interrupt();
+    _BIS_SR(LPM0_bits + GIE); 
 }
 
-// Interrupt of CCR0 data module of TA0
 #pragma vector=TIMER0_A0_VECTOR
-__interrupt void TA0CCR0_isr(void) {
-  // Toggle RED LED
-  P1OUT ^= BIT6;
+__interrupt void Timer_A0_ISR(void) {
+    P1OUT ^= BIT6;  
 }
 
-// Interrupt of CCR0 data module of TA1
-#pragma vector=TIMER1_A1_VECTOR
-__interrupt void TA1CCR0_isr(void) {
-  // Toggle GREEN LED
-  P1OUT ^= BIT0;
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer_A1_ISR(void) {
+    P1OUT ^= BIT0; 
 }
